8d88170766f6c76bbd2e0b19c90892490f329e48
crue-lty <36682589+crue-lty@users.noreply.github.com>
2026-07-10 23:31:06 +0200

    Working on embedded titles being more readable..

--- download_file.py
+++ download_file.py
@@ -1,6 +1,5 @@
 import ctypes, os, requests, time
 from dotenv import load_dotenv
-from urllib.parse import unquote
 
 load_dotenv() # Opens the window
 
@@ -12,13 +11,12 @@ webhook_url = os.getenv('TOKEN')
 url = 'https://commons.wikimedia.org/wiki/Special:Random'
 
 # ~~~~~~~~~~~~~~~~~ Sends a new link X times ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-for i in range(10):
+for i in range(1):
   r = requests.get(url, headers=headers, allow_redirects=False)
   link = r.headers['Location']
   if link.startswith('//') == True:
     link = 'https:' + link
   location = link.split('/')[4]
-  embed_title = unquote(location.replace('_', ' '))
   new_link = 'https://commons.wikimedia.org/w/rest.php/v1/file/' + location
 
   # ~~~~~~~~~~~~~~~~~ Pull a new link out of the hat, show the status code ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -28,7 +26,7 @@ for i in range(10):
     time.sleep(10)
     continue
   data = response.json()
-  ready_link = data['preferred']['url']
+  ready_link = data['original']['url']
 
   # ~~~~~~~~~~~~~~~~~ File formats ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   format_audio = ''
@@ -44,29 +42,33 @@ for i in range(10):
   # ~~~~~~~~~~~~~~~~~ Counter; adds https: to the links as well ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   if ready_link.startswith('https') == False:
     ready_link = 'https:' + ready_link
+
+  file_request = requests.get(ready_link)
+  file = file_request.content
+
   print(str(i + 1) + ' | Ready link: ' + ready_link)
   print(str(i + 1) + ' | Link: ' + link)
-  print(str(i + 1) + ' | This is the embedded title: ' + embed_title)
   time.sleep(4)
 
   # ~~~~~~~~~~~~~~~~~ Webhook ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  discord_message = requests.post(webhook_url, json={
-    'embeds': [
-      {
-      'title': embed_title,
-      'url': link
-      },
-      {
-      'image': {
-        'url': ready_link
-      }
-      }
-    ]
-  })
 
-  if discord_message.status_code != 204:
-    print(discord_message.json())
-  print(discord_message.status_code)
+  # discord_message = requests.post(webhook_url, json={
+  #   'embeds': [
+  #     {
+  #     'title': location.replace('_', '\\_'),
+  #     'url': link
+  #     },
+  #     {
+  #     'image': {
+  #       'url': ready_link
+  #     }
+  #     }
+  #   ]
+  # })
+
+  # if discord_message.status_code != 204:
+  #   print(discord_message.json())
+  # print(discord_message.status_code)
 
 MessageBox = ctypes.windll.user32.MessageBoxW
 MessageBox(None, ':)', 'Check me out!!!', 0)
